8d7a68faa9eb7fd32207c244552774bfaa2d26a3
Zaidi <Zaidi@ZAIDI-PC>
2019-10-19 23:25:01 +0530

    Add project files.

---
+++ Technical Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Technical_Test.Startup))]
+namespace Technical_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
